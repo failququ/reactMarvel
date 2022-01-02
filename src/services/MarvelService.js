9abeb05305f4deb7369f05ad8c617e370a5833d3
failququ <example@gmail.com>
2022-01-02 11:38:43 +0300

    added get requests, spinner, error

---
+++ src/services/MarvelService.js
@@ -0,0 +1,38 @@
+
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = '5d90fb362dc0e9496e056f8d1699ac16';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if(!res.ok) {
+            throw new Error(`Couldn't fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?&apikey=${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (character) => {
+        return {
+                    name: character.name,
+                    description:character.description,
+                    thumbnail: character.thumbnail.path + '.' + character.thumbnail.extension,
+                    homepage: character.urls[0].url,
+                    wiki: character.urls[1].url
+        }
+    }
+}
+
+export default MarvelService;
